23d95e05260b40203c709eb44113e8f0a672de9f
SneakyTactician <sneakytactician@outlook.com>
2017-02-13 16:08:12 -0600

    Working on a new command.

---
+++ CommandEverything/CommandEverything/Framework/Commands/ChangeDirectory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandEverything.Framework.Commands
+{
+    /// <summary>
+    /// Changes the directory this program is operating out of.
+    /// </summary>
+    public class ChangeDirectory
+    {
+    }
+}
