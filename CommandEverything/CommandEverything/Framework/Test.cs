7ec10f8521a48ce08528a7f7963a36d2c150aebf
SneakyTactician <sneakytactician@outlook.com>
2017-02-14 16:02:31 -0600

    organizing.

---
+++ CommandEverything/CommandEverything/Framework/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandEverything.Framework
+{
+    public class Test
+    {
+        enum Region { Europe, Alps, Benelux, Germany, SpainPortugal, France, Greece, Italy, Poland, Scandinavia };
+        enum CountryCode { Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
+    }
+}
