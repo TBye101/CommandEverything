38098b1f3e59b9eb15426e831c61775985c33263
SneakyTactician <sneakytactician@outlook.com>
2017-02-14 16:04:36 -0600

    Inheriting.

---
+++ CommandEverything/CommandEverything/Framework/WIP/FreezeProgram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandEverything.Framework.Commands
+{
+    /// <summary>
+    /// Freezes the specified running program.
+    /// </summary>
+    public class FreezeProgram : ICommand
+    {
+        public string GetHelp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run(string Input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ShouldRunThisCommand(string Input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
