a18464de3d8487864e45079996470b6f85ec232f
SneakyTactician <sneakytactician@outlook.com>
2017-02-22 16:42:12 -0600

    Documentation.

---
+++ CommandEverything/CommandEverything/Framework/CMD/Interaction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandEverything.Framework.CMD
+{
+    /// <summary>
+    /// This class handles all interaction between this program and the system CMD.
+    /// </summary>
+    public class Interaction
+    {
+        public void Go()
+        {
+            string output = string.Empty;
+            string error = string.Empty;
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd", "/c dir");
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            processStartInfo.UseShellExecute = false;
+
+            Process process = Process.Start(processStartInfo);
+            using (StreamReader streamReader = process.StandardOutput)
+            {
+                output = streamReader.ReadToEnd();
+            }
+
+            using (StreamReader streamReader = process.StandardError)
+            {
+                error = streamReader.ReadToEnd();
+            }
+
+            Console.WriteLine("The following output was detected:");
+            Console.WriteLine(output);
+
+            if (!string.IsNullOrEmpty(error))
+            {
+                Console.WriteLine("The following error was detected:");
+                Console.WriteLine(error);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ CommandEverything/CommandEverything/Framework/Util/Text/ConsoleWriter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandEverything.Framework.Util
+{
+    /// <summary>
+    /// Knows how to do more advanced kinds of output to the console.
+    /// </summary>
+    public class ConsoleWriter
+    {
+        /// <summary>
+        /// Writes the message to the console.
+        /// </summary>
+        /// <param name="ToWrite">The message</param>
+        /// <param name="Program">Name of the program this message should be displayed as coming from.</param>
+        public void WriteLine(Writable ToWrite, string Program)
+        {
+
+        }
+
+        /// <summary>
+        /// Writes the messages to the console.
+        /// </summary>
+        /// <param name="ToWrite">The message</param>
+        /// <param name="Program">Name of the program this message should be displayed as coming from.</param>
+        public void WriteAll(Writable[] ToWrite, string Program)
+        {
+
+        }
+
+        /// <summary>
+        /// Writes the messages to the console.
+        /// </summary>
+        /// <param name="ToWrite">The message</param>
+        /// <param name="Program">Name of the program this message should be displayed as coming from.</param>
+        public void WriteAll(List<Writable> ToWrite, string Program)
+        {
+
+        }
+    }
+}
