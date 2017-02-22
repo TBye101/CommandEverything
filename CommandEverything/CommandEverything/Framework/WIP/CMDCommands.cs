12744f513135bc49a29dc84772030582d2e9f26f
SneakyTactician <sneakytactician@outlook.com>
2017-02-22 17:14:45 -0600

    Finished work on the CMD commands.

---
+++ CommandEverything/CommandEverything/Framework/WIP/CMDCommands.cs
@@ -0,0 +1,37 @@
+﻿using CommandEverything.Framework.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandEverything.Framework.WIP
+{
+    /// <summary>
+    /// Runs CMD commands if they are found.
+    /// </summary>
+    public class CMDCommands : ICommand
+    {
+        public string GetHelp()
+        {
+            return "Used to run a CMD command from this command prompt. Just do 'CMD' followed by your arguments to CMD and it will receive your command";
+        }
+
+        public string GetName()
+        {
+            return "CMD";
+        }
+
+        public void Run(string Input)
+        {
+            Utility.RemoveWordFromString(Input, "CMD");
+        }
+
+        public bool ShouldRunThisCommand(string Input)
+        {
+            string[] Valid = { "CMD" };
+
+            return Utility.DoesStringContain(Input, Valid);
+        }
+    }
+}
