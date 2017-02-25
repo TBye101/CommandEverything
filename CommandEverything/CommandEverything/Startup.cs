691c1dbc8de9b6db3bcb53296b53333f3246a704
SneakyTactician <sneakytactician@outlook.com>
2017-02-25 14:24:05 -0600

    Changed some settings.

---
+++ CommandEverything/CommandEverything/Startup.cs
@@ -0,0 +1,39 @@
+﻿using CommandEverything.Framework.Util;
+using System;
+using System.IO;
+
+namespace CommandEverything
+{
+    /// <summary>
+    /// Does some startup logic.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Runs some startup logic.
+        /// </summary>
+        public void Go()
+        {
+
+        }
+
+        /// <summary>
+        /// Verifies that a folder exists. If it doesn't, it is created.
+        /// </summary>
+        /// <param name="Path"></param>
+        public void VerifyFolder(string Path)
+        {
+            try
+            {
+                if (!Directory.Exists(Path))
+                {
+                    Directory.CreateDirectory(Path);
+                }
+            }
+            catch (Exception TheException)
+            {
+                Error.Report(TheException);
+            }
+        }
+    }
+}
