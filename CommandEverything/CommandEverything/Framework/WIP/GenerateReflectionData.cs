058757bc10469ee18a5f34de124cdb5dca6a1718
SneakyTactician <sneakytactician@outlook.com>
2017-02-25 16:41:28 -0600

    Thought of a new idea.

---
+++ CommandEverything/CommandEverything/Framework/WIP/GenerateReflectionData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandEverything.Framework.WIP
+{
+    /// <summary>
+    /// Generates as much data as it can about the specified dll/exe.
+    /// </summary>
+    public class GenerateReflectionData
+    {
+    }
+}
