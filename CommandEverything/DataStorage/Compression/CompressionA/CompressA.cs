7a5f38ea7502e506b10891bd6db12ee8da570263
SneakyTactician <sneakytactician@outlook.com>
2017-03-03 16:52:02 -0600

    WIP compression

--- CommandEverything/DataStorage/Compression/CompressionA/CompressA.cs
+++ CommandEverything/DataStorage/Compression/CompressionA/CompressA.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace DataStorage.Compression.CompressionA
 {
     /// <summary>
-    /// Used to compress strings.
+    /// Used to compress and decompress strings.
     /// </summary>
     public static class CompressA
     {
         /// <summary>
         /// Stores the current compression dictionary.
         /// </summary>
-        private static Dictionary<string, string> CompressionDictionary = new Dictionary<string, string>();
+        private static Dictionary<string, string> CompressionDictionary = DictionaryParser.ParseFile(Resources.CompressionDictionaryA.ToString());
 
         /// <summary>
         /// Compresses the given string and returns it.
@@ -24,6 +24,26 @@ namespace DataStorage.Compression.CompressionA
         public static string Compress(string ToCompress)
         {
             string Binary = Encoding.ASCII.GetBytes(ToCompress).ToString();
+
+            int i = 0;
+            int size = CompressionDictionary.Count;
+
+            while (i != size)
+            {
+                string item = CompressionDictionary.ElementAt(size - 1).Key;
+                //search for it and replace it.
+                i++;
+            }
+
+            foreach (string item in CompressionDictionary.ElementAt Reverse())
+            {
+
+            }
+            //Write compression logic here
+        }
+
+        public static string Decompress(string ToDecompress)
+        {
 
         }
     }
