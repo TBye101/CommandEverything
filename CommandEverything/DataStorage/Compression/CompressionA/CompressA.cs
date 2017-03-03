9aa20925403d8a41e4fe640b2d4781000900db27
SneakyTactician <sneakytactician@outlook.com>
2017-03-03 16:23:03 -0600

    Created a compression dictionary.

---
+++ CommandEverything/DataStorage/Compression/CompressionA/CompressA.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStorage.Compression.CompressionA
+{
+    /// <summary>
+    /// Used to compress strings.
+    /// </summary>
+    public class CompressA
+    {
+        /// <summary>
+        /// Compresses the given string and returns it.
+        /// </summary>
+        /// <param name="ToCompress"></param>
+        /// <returns></returns>
+        public string Compress(string ToCompress)
+        {
+
+        }
+    }
+}
