5a8155b8d7a8cd43cc014b46d1ce3a8e0a5e9dbe
SneakyTactician <sneakytactician@outlook.com>
2017-06-27 08:56:19 -0500

    Fixed the shutdown command.

--- CommandEverything/CommandEverything/Framework/Commands/ShutdownCommand.cs
+++ CommandEverything/CommandEverything/Framework/Commands/ShutdownCommand.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Management;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace CommandEverything.Framework.Commands
 {
@@ -25,22 +26,8 @@ namespace CommandEverything.Framework.Commands
 
         public void Run(string Input)
         {
-            ManagementBaseObject mboShutdown = null;
-            ManagementClass mcWin32 = new ManagementClass("Win32_OperatingSystem");
-            mcWin32.Get();
-
-            // You can't shutdown without security privileges
-            mcWin32.Scope.Options.EnablePrivileges = true;
-            ManagementBaseObject mboShutdownParams =
-                     mcWin32.GetMethodParameters("Win32Shutdown");
-
-            // Flag 1 means we want to shut down the system. Use "2" to reboot.
-            mboShutdownParams["Flags"] = "1";
-            mboShutdownParams["Reserved"] = "0";
-            foreach (ManagementObject manObj in mcWin32.GetInstances())
-            {
-                mboShutdown = manObj.InvokeMethod("Win32Shutdown", mboShutdownParams, null);
-            }
+            ShutdownUtil a = new ShutdownUtil();
+            a.ExitWindows(ShutdownUtil.EWX_SHUTDOWN);
         }
 
         public bool ShouldRunThisCommand(string Input)
