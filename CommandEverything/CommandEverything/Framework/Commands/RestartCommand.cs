138599872a38fffd962f21dca2e8c6285029ce41
SneakyTactician <sneakytactician@outlook.com>
2017-06-27 08:57:10 -0500

    Fixed the restart command.

--- CommandEverything/CommandEverything/Framework/Commands/RestartCommand.cs
+++ CommandEverything/CommandEverything/Framework/Commands/RestartCommand.cs
@@ -25,22 +25,8 @@ namespace CommandEverything.Framework.Commands
 
         public void Run(string Input)
         {
-            ManagementBaseObject mboShutdown = null;
-            ManagementClass mcWin32 = new ManagementClass("Win32_OperatingSystem");
-            mcWin32.Get();
-
-            // You can't shutdown without security privileges
-            mcWin32.Scope.Options.EnablePrivileges = true;
-            ManagementBaseObject mboShutdownParams =
-                     mcWin32.GetMethodParameters("Win32Shutdown");
-
-            // Flag 1 means we want to shut down the system. Use "2" to reboot.
-            mboShutdownParams["Flags"] = "2";
-            mboShutdownParams["Reserved"] = "0";
-            foreach (ManagementObject manObj in mcWin32.GetInstances())
-            {
-                mboShutdown = manObj.InvokeMethod("Win32Shutdown", mboShutdownParams, null);
-            }
+            ShutdownUtil a = new ShutdownUtil();
+            a.ExitWindows(ShutdownUtil.EWX_RESTARTAPPS);
         }
 
         public bool ShouldRunThisCommand(string Input)
