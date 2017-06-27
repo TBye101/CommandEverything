f9a251c01286d8f8f118bc6bda8c2de9aa6180e7
SneakyTactician <sneakytactician@outlook.com>
2017-06-27 12:34:57 -0500

    Compression working properly, deleted useless project.

--- CommandEverything/CommandEverything/Framework/WIP/CompressCommand.cs
+++ CommandEverything/CommandEverything/Framework/WIP/CompressCommand.cs
@@ -1,9 +1,9 @@
 ﻿using CommandEverything.Framework.Util;
 using CommandEverything.Framework.Util.Text;
-using DataStorage.Compression.CompressionA;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,8 +26,8 @@ namespace CommandEverything.Framework.WIP
         public void Run(string Input)
         {
             OpenFileDialog dlg = new OpenFileDialog();
-            dlg.FileName = "File"; // Default file name
-            dlg.DefaultExt = ".txt"; // Default file extension
+            dlg.FileName = "File";
+            dlg.DefaultExt = ".txt";
 
             // Show open file dialog box
             DialogResult result = dlg.ShowDialog();
@@ -38,12 +38,13 @@ namespace CommandEverything.Framework.WIP
                 // Open document
                 string[] file = File.ReadAllLines(dlg.FileName, Encoding.Default);
                 ConsoleWriter.WriteLine("Opened file");
-                string compressed = CompressA.Compress(Utility.ConvertArray(file));
+                string compressed = CompressString(Utility.ConvertArray(file));
                 ConsoleWriter.WriteLine("Compressed file");
 
                 SaveFileDialog sv = new SaveFileDialog();
                 sv.FileName = dlg.FileName;
-                sv.DefaultExt = "ce";
+                sv.DefaultExt = ".ce";
+
                 DialogResult res = sv.ShowDialog();
 
                 if (res == DialogResult.OK)
@@ -52,6 +53,7 @@ namespace CommandEverything.Framework.WIP
                     sw.WriteLine(compressed);
                     sw.Flush();
                     sw.Dispose();
+                    ConsoleWriter.WriteLine("Compressed file saved");
                 }
             }
             else
@@ -64,6 +66,56 @@ namespace CommandEverything.Framework.WIP
         {
             string[] Valid = { "compress", "compress file", "shrink file"};
             return Utility.DoesStringContain(Input, Valid);
+        }
+
+        /// <summary>
+        /// Compresses the string.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <returns></returns>
+        private string CompressString(string text)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+            var memoryStream = new MemoryStream();
+            using (var gZipStream = new GZipStream(memoryStream, CompressionMode.Compress, true))
+            {
+                gZipStream.Write(buffer, 0, buffer.Length);
+            }
+
+            memoryStream.Position = 0;
+
+            var compressedData = new byte[memoryStream.Length];
+            memoryStream.Read(compressedData, 0, compressedData.Length);
+
+            var gZipBuffer = new byte[compressedData.Length + 4];
+            Buffer.BlockCopy(compressedData, 0, gZipBuffer, 4, compressedData.Length);
+            Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gZipBuffer, 0, 4);
+            return Convert.ToBase64String(gZipBuffer);
+        }
+
+        /// <summary>
+        /// Decompresses the string.
+        /// </summary>
+        /// <param name="compressedText">The compressed text.</param>
+        /// <returns></returns>
+        private string DecompressString(string compressedText)
+        {
+            byte[] gZipBuffer = Convert.FromBase64String(compressedText);
+            using (var memoryStream = new MemoryStream())
+            {
+                int dataLength = BitConverter.ToInt32(gZipBuffer, 0);
+                memoryStream.Write(gZipBuffer, 4, gZipBuffer.Length - 4);
+
+                var buffer = new byte[dataLength];
+
+                memoryStream.Position = 0;
+                using (var gZipStream = new GZipStream(memoryStream, CompressionMode.Decompress))
+                {
+                    gZipStream.Read(buffer, 0, buffer.Length);
+                }
+
+                return Encoding.UTF8.GetString(buffer);
+            }
         }
     }
 }
