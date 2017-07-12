6363e70a34cb56b330519e304aafae3860c21c98
SneakyTactician <sneakytactician@outlook.com>
2017-07-12 09:35:45 -0500

    Almost done with the start command.

--- CommandEverything/CommandEverythingCPP/CommandStart.cpp
+++ CommandEverything/CommandEverythingCPP/CommandStart.cpp
@@ -10,3 +10,22 @@ CommandStart::CommandStart()
 CommandStart::~CommandStart()
 {
 }
+
+bool CommandStart::ShouldRunThisCommand(ParsedCommand *Parsed)
+{
+	return (Parsed->Words->at(0) == "start");
+}
+
+void CommandStart::Run(ParsedCommand * Parsed)
+{
+}
+
+string * CommandStart::GetName()
+{
+	return new string("Start");
+}
+
+string * CommandStart::GetHelp()
+{
+	return new string("Starts a new instance of this command line.");
+}
--- CommandEverything/CommandEverythingCPP/CommandStart.h
+++ CommandEverything/CommandEverythingCPP/CommandStart.h
@@ -1,8 +1,19 @@
 #pragma once
-class CommandStart
+
+/// <summary>
+/// Used to start a new instance of our command line.
+/// </summary>
+class CommandStart : public ICommand
 {
 public:
 	CommandStart();
 	~CommandStart();
-};
 
+	bool ShouldRunThisCommand(ParsedCommand* Parsed);
+
+	void Run(ParsedCommand* Parsed);
+
+	string* GetName();
+
+	string* GetHelp();
+};
