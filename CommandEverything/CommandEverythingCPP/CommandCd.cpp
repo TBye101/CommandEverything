a8c9d1f5a6b4d0ae9a11f1c6b1ab6d23c759d301
SneakyTactician <sneakytactician@outlook.com>
2017-07-16 22:38:48 -0500

    Added the cd command class.

---
+++ CommandEverything/CommandEverythingCPP/CommandCd.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "CommandCd.h"
+
+
+CommandCd::CommandCd()
+{
+}
+
+
+CommandCd::~CommandCd()
+{
+}
---
+++ CommandEverything/CommandEverythingCPP/CommandCd.h
@@ -0,0 +1,8 @@
+#pragma once
+class CommandCd
+{
+public:
+	CommandCd();
+	~CommandCd();
+};
+
