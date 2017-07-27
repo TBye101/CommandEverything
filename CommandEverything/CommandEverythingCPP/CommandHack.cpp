a79f409b5f6bdb9daf0133bb0a849274e50908cd
SneakyTactician <sneakytactician@outlook.com>
2017-07-27 12:14:58 -0500

    Think it works, gonna test it.

--- CommandEverything/CommandEverythingCPP/CommandHack.cpp
+++ CommandEverything/CommandEverythingCPP/CommandHack.cpp
@@ -62,26 +62,25 @@ void CommandHack::Attempt(string* attempt)
 	{
 		Sleep(500);
 	}
-	ip.type = INPUT_KEYBOARD;
-	ip.ki.time = 0;
-	ip.ki.dwFlags = KEYEVENTF_UNICODE; // Specify the key as a Unicode character
-	ip.ki.wScan = VK_RETURN; // Which key press to simulate
-	ip.ki.wVk = 0;
-	ip.ki.dwExtraInfo = 0;
-	SendInput(1, &ip, sizeof(INPUT));
-	ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
-	SendInput(1, &ip, sizeof(INPUT));
+
+	keybd_event(VK_RETURN, '5A', 0, 0);
+	keybd_event(VK_RETURN, '5A', KEYEVENTF_KEYUP, 0);
+
+	register unsigned __int8 ab = 0;
+
+	while (ab != this->Length)
+	{
+		keybd_event(VK_BACK, '66', 0, 0);
+		keybd_event(VK_BACK, '66', KEYEVENTF_KEYUP, 0);
+		ab++;
+	}
 }
 
 void CommandHack::Crack()
 {
-	//register char attempting[128];
-
 	//32-137 
 	//Acquire common characters. 
-
 	register unsigned __int8 i = 32;
-
 	while (i != 128)
 	{
 		this->Chars[i - 32] = i;
