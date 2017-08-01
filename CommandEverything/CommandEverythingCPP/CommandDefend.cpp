9dbdf3368f36bfa3352390ae215f7d2723f99033
SneakyTactician <sneakytactician@outlook.com>
2017-08-01 09:53:04 -0500

    Deleted 2 libs that were not working anyways.

--- CommandEverything/CommandEverythingCPP/CommandDefend.cpp
+++ CommandEverything/CommandEverythingCPP/CommandDefend.cpp
@@ -1,8 +1,6 @@
 #include "stdafx.h"
 #include "CommandDefend.h"
 
-
-
 CommandDefend::CommandDefend()
 {
 }
---
+++ CommandEverything/CommandEverythingCPP/CommandKill.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "ICommand.h"
+
+class CommandKill
+{
+public:
+	CommandKill();
+	~CommandKill();
+};
+
