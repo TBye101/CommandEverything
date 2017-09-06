6b9f276e06c5e675fa1bce08ce8fa189bea2af15
SneakyTactician <sneakytactician@outlook.com>
2017-09-06 18:28:21 -0500

    Rough draft of lock all.

--- CommandEverything/CommandEverythingCPP/CommandDefend.cpp
+++ CommandEverything/CommandEverythingCPP/CommandDefend.cpp
@@ -78,12 +78,29 @@ bool CommandDefend::DoesProcessExistInList(unsigned int pID, unsigned int number
 
 void CommandDefend::DefendStart()
 {
-	if (!this->DefenseThread)
-	{
-		delete this->DefenseThread;
-	}
+	register unsigned long mydrives = 100; // buffer length
+	register wchar_t lpBuffer[100]; // buffer for drive string storage
+	register unsigned long test = GetLogicalDriveStrings(mydrives, lpBuffer);
+	register unsigned __int32 drives = test / 4;
+	string drive = "";
+	char strbuffer[64];
 
-	this->DefenseThread = new thread(&CommandDefend::Defend, this);
+
+	//if (!this->DefenseThread)
+	//{
+	//	delete this->DefenseThread;
+	//}
+	//this->DefenseThread = new thread(&CommandDefend::Defend, this);
+	
+	for (register unsigned __int8 i = 0; i < drives; i++)
+	{
+		wctomb(strbuffer, lpBuffer[i * 4]);
+		drive = strbuffer[0];
+		drive.append("://");
+		Console->WriteLine(&("Locking files on drive " + drive));
+		this->LockFiles(Utility->ToCharStar(&drive), 0);
+		Console->WriteLine("Done locking files on drive");
+	}
 }
 
 void CommandDefend::DefendStop()
@@ -160,4 +177,46 @@ void CommandDefend::Defend()
 			}
 		}
 	}
+}
+
+void CommandDefend::LockFiles(char* name, int indent = 0)
+{
+	register unsigned __int64 totalFiles = Utility->GraphicCalculateFilesIn(name, indent);
+
+	register DIR *dir;
+	register struct dirent *entry;
+
+	if (!(dir = opendir(name)))
+	{
+		return;
+	}
+
+	while ((entry = readdir(dir)) != NULL)
+	{
+		if (ControlCPressed)
+		{
+			break;
+		}
+		if (entry->d_type == DT_DIR)
+		{
+			char path[MAX_PATH];
+			if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+			{
+				continue;
+			}
+			snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);
+			this->LockFiles(path, indent + 2);
+		}
+		else
+		{
+			//Do stuff per each file
+			char* convert = entry->d_name;
+			wchar_t* wtext;
+			mbstowcs(wtext, convert, strlen(convert) + 1);//Plus null
+			LPWSTR ptr = wtext;
+
+			CreateFile(ptr, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+		}
+	}
+	closedir(dir);
 }
