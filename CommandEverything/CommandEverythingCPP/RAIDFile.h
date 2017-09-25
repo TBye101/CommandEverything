27d80cdadbc0867e5f57d9524236685da1fe06f6
SneakyTactician <sneakytactician@outlook.com>
2017-09-25 11:38:30 -0500

    RAID now creates the log files.

--- CommandEverything/CommandEverythingCPP/RAIDFile.h
+++ CommandEverything/CommandEverythingCPP/RAIDFile.h
@@ -1,10 +1,28 @@
 #pragma once
+
+
+
 /// <summary>
 /// Used to create a file on all drives, and write to whichever drive is ready first.
 /// </summary>
 class RAIDFile
 {
 public:
+
+	/// <summary>
+	/// Holds all the files.
+	/// </summary>
+	ofstream* logArray;
+
+	/// <summary>
+	/// The number of streams in logArray.
+	/// </summary>
+	unsigned __int8 logArraySize;
+
+	/// <summary>
+	/// The index of the next file to write to.
+	/// </summary>
+	unsigned __int8 logNext = 0;
 
 	/// <summary>
 	/// Creates a file on all drives.
@@ -15,4 +33,16 @@ public:
 	/// <param name="arraySize">The size of the rootPathArray.</param>
 	RAIDFile(wstring* rootPathArray, unsigned __int8 arraySize, const char* path, const char* fileName);
 	~RAIDFile();
+
+	/// <summary>
+	/// Logs a line to a file. 
+	/// </summary>
+	/// <param name="str"></param>
+	void writeLine(const char* str);
+private:
+	/// <summary>
+	/// Gets the time in string format.
+	/// </summary>
+	/// <returns></returns>
+	string* GetTime();
 };
